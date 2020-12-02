b66ce844643abe4e81b6522c10b24774f640e10d
Adam Figueroa <admiralfiggins@gmail.com>
2020-12-01 19:11:54 -0800

    Final commit

--- src/store.js
+++ src/store.js
@@ -1,38 +1,27 @@
-import item from './item';
 
 const items = [];
+let error = null;
 let hideCheckeditems = false;
 
 const findById = function (id) {
   return this.items.find(currentItem => currentItem.id === id);
 };
 
-const addItem = function (name) {
-  try {
-    item.validateName(name);
-    this.items.push(item.create(name));
-  } catch (e) {
-    console.log(e.message);
-  }
+const setError = function (error) {
+  this.error = error;
 };
 
-const findAndToggleChecked = function (id) {
-  const currentItem = this.findById(id);
-  currentItem.checked = !currentItem.checked;
-};
-
-const findAndUpdateName = function (id, name) {
-  try {
-    item.validateName(name);
-    const currentItem = this.findById(id);
-    currentItem.name = name;
-  } catch (e) {
-    console.log('Cannot update name: ' + e.message);
-  }
+const addItem = function (item) {
+  this.items.push(item);
 };
 
 const findAndDelete = function (id) {
   this.items = this.items.filter(currentItem => currentItem.id !== id);
+};
+
+const findAndUpdate = function (id, newData) {
+  const currentItem = this.findById(id);
+  Object.assign(currentItem, newData);
 };
 
 const toggleCheckedFilter = function () {
@@ -44,8 +33,8 @@ export default {
   hideCheckeditems,
   findById,
   addItem,
-  findAndToggleChecked,
-  findAndUpdateName,
   findAndDelete,
-  toggleCheckedFilter
+  toggleCheckedFilter,
+  findAndUpdate,
+  setError
 };
